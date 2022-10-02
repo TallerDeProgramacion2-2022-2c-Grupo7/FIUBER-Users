2235a93f09f3a9a3b6b98468f7c7e70bd7d96336
Matías Huenul <matias.huenul.07@gmail.com>
2022-10-02 16:31:05 -0300

    Fix deployment

--- src/common/firebase_credentials.py
+++ src/common/firebase_credentials.py
@@ -1,14 +1,6 @@
 import os
+import json
+from base64 import b64decode
 
-admin_credentials = {
-    "type": "service_account",
-    "project_id": os.environ["FIREBASE_PROJECT_ID"],
-    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
-    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": os.environ["FIREBASE_CLIENT_ID"],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"],
-}
+_json_str = b64decode(os.environ["FIREBASE_CREDENTIALS"]).decode()
+admin_credentials = json.loads(_json_str)
